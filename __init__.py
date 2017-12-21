86c4852d7dd8256114ab4f5e6a609c1a77c4147d
Alexander Shepetko <a@shepetko.com>
2017-12-21 16:58:56 +0200

    Minor update 1.2.2

--- __init__.py
+++ __init__.py
@@ -6,15 +6,28 @@ __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
 
-def plugin_load():
-    from pytsite import lang
+def _register_assetman_resources():
     from plugins import assetman
 
-    lang.register_package(__name__)
+    if not assetman.is_package_registered(__name__):
+        assetman.register_package(__name__)
+        assetman.t_js(__name__)
+        assetman.js_module('pytsite-reload', __name__ + '@js/pytsite-reload')
 
-    assetman.register_package(__name__)
-    assetman.t_js(__name__)
-    assetman.js_module('pytsite-reload', __name__ + '@js/pytsite-reload')
+    return assetman
+
+
+def plugin_install():
+    assetman = _register_assetman_resources()
+    assetman.build(__name__)
+    assetman.build_translations()
+
+
+def plugin_load():
+    from pytsite import lang
+
+    lang.register_package(__name__)
+    _register_assetman_resources()
 
 
 def plugin_load_uwsgi():
@@ -27,11 +40,3 @@ def plugin_load_uwsgi():
     http_api.handle('POST', 'reload', _http_api_controllers.PostReload, 'reload_ui@reload')
 
     permissions.define_permission('reload_ui@reload', 'reload_ui@reload_application_permission', 'app')
-
-
-def plugin_install():
-    from plugins import assetman
-
-    plugin_load()
-    assetman.build(__name__)
-    assetman.build_translations()
